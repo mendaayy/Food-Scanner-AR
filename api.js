7a90ecf6196ba1786cd15e383cdcef03918f7041
I488547 <i488547@fhict.nl>
2023-04-03 12:55:27 +0200

    working api

--- api.js
+++ api.js
@@ -1,24 +1,33 @@
-const FormData = require("form-data");
-const fs = require("fs");
-const https = require('https');
+const apiUrl = 'https://api.logmeal.es/v2/image/segmentation/complete/v1.0?language=eng';
+const token = 'dd5e0ea7e5d892464cce19c83609f2d1b341720a';
+const fileInput = document.querySelector('#file-input');
+let uniqueFoods = new Set();
 
-var form = new FormData();
-form.append('image', fs.createReadStream('pasta.jpg'));
+fileInput.addEventListener('change', function() {
+    const file = fileInput.files[0];
+    const form = new FormData();
+    form.append('image', file);
 
-var headers = form.getHeaders();
-headers['Authorization'] = 'Bearer dd5e0ea7e5d892464cce19c83609f2d1b341720a';
-
-const options = {
-    hostname: 'api.logmeal.es',
-    path: '/v2/recognition/dish',
+    fetch(apiUrl, {
     method: 'POST',
-    headers: headers,
-};
+    headers: {
+        'Authorization': `Bearer ${token}`
+    },
+    body: form
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); 
 
-const req = https.request(options, (res) => {
-    res.on('data', (d) => {
-        process.stdout.write(d);
+        data.segmentation_results.forEach(segment => {
+            let result = segment.recognition_results[0].name;
+            if (!uniqueFoods.has(result)) { // Check if food name is already in the list
+            uniqueFoods.add(result);
+            document.getElementById('food-list').innerHTML += `<li>${result}</li>`;
+            }
+        });
+    })
+    .catch(error => {
+        console.error(error); 
     });
 });
-
-form.pipe(req);
